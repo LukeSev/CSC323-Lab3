150be0becf9041b10dc0cc3e8b3b591cf3970fe3
LukeSev <lloughner00@gmail.com>
2023-03-05 11:25:14 -0800

    Testing Ciphertext Added

--- Task_I/PaddingOracleAttack.py
+++ Task_I/PaddingOracleAttack.py
@@ -18,9 +18,26 @@ def eavesdrop():
     # Ciphertext appears on second line
     return output[1]
 
+def test_ciphertext(ciphertext):
+    # Takes in ciphertext as hex string and returns 1 if valid and 0 otherwise
+    # First word in request's text is either 404 (valid decryption) or 403 (invalid decryption)
+    r = requests.get(base_url + "/?enc=" + ciphertext).text.split(' ')
+    return int(r[0]) - 403
+
 def main():
     ciphertext = eavesdrop()
-    print(ciphertext)
+    print("Testing ciphertext: {}".format(ciphertext))
+    if(test_ciphertext(ciphertext)):
+        print("Valid decryption!\n")
+    else:
+        print("Invalid decryption :(\n")
+    invalid_ctext = "A" * 15
+    print("Testing ciphertext: {}".format(invalid_ctext))
+    if(test_ciphertext(invalid_ctext)):
+        print("Valid decryption!\n")
+    else:
+        print("Invalid decryption :(\n")
+
 
 if __name__ == '__main__':
     main()
